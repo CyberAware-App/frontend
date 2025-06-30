bf88dec340f3061557e0b127f5351e236ba5e226
Ryan-Zayne <phoenixastro97@gmail.com>
2025-06-30 20:45:15 +0100

    .

--- app/(primary)/layout.tsx
+++ app/(primary)/layout.tsx
@@ -1,5 +1,5 @@
 function PrimaryLayout({ children }: { children: React.ReactNode }) {
-	return <div className="bg-app-dark flex min-h-svh w-full flex-col items-center">{children}</div>;
+	return <div className="flex min-h-svh w-full flex-col items-center">{children}</div>;
 }
 
 export default PrimaryLayout;
