c328b7af757e4ed18deca75870dfca7dc7821d68
Ryan-Zayne <phoenixastro97@gmail.com>
2025-06-30 20:39:54 +0100

    feat: set up colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,24 @@
 import type { Metadata } from "next";
-import { Albert_Sans, Inter } from "next/font/google";
+import { Work_Sans } from "next/font/google";
 import { cnJoin } from "../lib/utils/cn";
-import "../tailwind.css";
 import { Providers } from "./Providers";
+import "../tailwind.css";
 
-const albertSans = Albert_Sans({
-	variable: "--font-albert-sans",
-	subsets: ["latin"],
-});
-
-const inter = Inter({
-	variable: "--font-inter",
+const workSans = Work_Sans({
+	variable: "--font-work-sans",
 	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
 	title: "CyberAware",
-	// description:
-	// 	"A secure platform to report incidents, threats, or safety concerns. anonymously and effectively.",
+	description:
+		"Learn how to protect your digital world in just 10 days. Designed for lecturers, staff, and students",
 };
 
 function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={cnJoin(albertSans.variable, inter.variable)}>
+			<body className={cnJoin(workSans.variable)}>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
--- components/common/Image.tsx
+++
@@ -1,6 +0,0 @@
-import type { InferProps } from "@zayne-labs/toolkit-react/utils";
-import NextImage from "next/image";
-
-export function Image(props: InferProps<typeof NextImage>) {
-	return <NextImage {...props} />;
-}
