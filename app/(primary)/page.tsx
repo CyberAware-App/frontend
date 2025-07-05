29f834611821a4d5a538f9b94ea13c9e782b8e9c
Ryan-Zayne <phoenixastro97@gmail.com>
2025-07-05 15:01:49 +0100

    feat: perfect hero section

--- app/(primary)/page.tsx
+++ app/(primary)/page.tsx
@@ -1,19 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { aecesLogo, eyeShield, unizikLogo } from "@/public/assets";
+import { aecesLogo, heading, unizikLogo } from "@/public/assets";
 import { Main } from "../-components";
 
 function HomePage() {
 	return (
-		<Main>
+		<Main className="gap-12">
 			<section className="mx-4 mt-4 flex flex-col gap-8 bg-white px-[45px] pt-[170px] pb-4">
-				<h1 className="text-center text-[36px] font-bold text-cyberaware-aeces-blue">
-					Be CyberAware.
-					<span className="inline-flex items-center">
-						Stay <Image src={eyeShield} alt="Eye Shield" className="mt-1 ml-2 size-6" /> Secure
-					</span>
-					.
-				</h1>
+				<Image src={heading} alt="Heading" className="mx-auto" />
 				<p className="text-center text-[16px] text-cyberaware-body-color">
 					Learn how to protect your digital world in just 10 days. Designed for lecturers, staff, and
 					students.
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,5 +1,5 @@
 export { default as aecesLogo } from "./aeces-logo.svg";
-export { default as eyeShield } from "./eye-shield.svg";
+export { default as heading } from "./heading.png";
 export { default as logoLarge } from "./logo-large.svg";
 export { default as logoSmall } from "./logo-small.svg";
 export { default as unizikLogo } from "./unizik-logo.svg";
