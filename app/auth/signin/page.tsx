b2fa98e749b963fddb039bafd16568231fb6b0b7
unknown <uzukevictoria@gmail.com>
2025-07-14 10:36:34 +0100

    login page

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@zayne-labs/ui-react/ui/form";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Main } from "@/app/-components";
+import { Button } from "@/components/ui/button";
+
+const SigninSchema = z.object({
+	email: z.email("Invalid email address"),
+	password: z.string().min(8, "Password must be at least 8 characters long"),
+});
+
+function SigninPage() {
+	const form = useForm({
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+		mode: "onTouched",
+		resolver: zodResolver(SigninSchema),
+	});
+
+	return (
+		<Main className="relative gap-11 px-4 py-[158px]">
+			<header className="flex flex-col gap-5">
+				<h1 className="text-[36px] font-bold text-white">Welcome Back </h1>
+				<p className="text-[14px] text-white">
+					Log In to continue your 10-day cybersecurity Journey
+				</p>
+				<p>
+					Don't have an Account?{" "}
+					<Link href="/auth/signup" className="text-white">
+						Create one
+					</Link>
+				</p>
+			</header>
+
+			<section>
+				<Form.Root methods={form} className="flex flex-col gap-2">
+					<Form.Field name="email">
+						<Form.Label className="text-white">Email address</Form.Label>
+						<Form.Input
+							placeholder="Enter email address"
+							className="h-[64px] border-2 border-cyberaware-neutral-gray-light px-8 text-base
+								text-white placeholder:text-white/50 data-invalid:border-red-600"
+						/>
+
+						<Form.ErrorMessage />
+					</Form.Field>
+
+					<Form.Field name="password">
+						<Form.Label className="text-white">Password</Form.Label>
+						<Form.Input
+							type="password"
+							placeholder="Enter password"
+							classNames={{
+								inputGroup: `h-[64px] border-2 border-cyberaware-neutral-gray-light px-8 text-base
+								text-white placeholder:text-white/50 data-invalid:border-red-600`,
+							}}
+						/>
+
+						<Form.ErrorMessage />
+					</Form.Field>
+
+					<Form.Submit asChild={true} className="mt-[66px]">
+						<Button className="h-[64px] max-w-[260px] self-end">Log In</Button>
+					</Form.Submit>
+				</Form.Root>
+			</section>
+		</Main>
+	);
+}
+
+export default SigninPage;
