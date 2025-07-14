f2af21c5e5ecaec9a773eb76b6515827358cd2e7
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2025-07-14 11:32:50 +0100

    fix: added mobile side bar on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,53 +1,57 @@
 import React from "react";
+import Image from "next/image";
 import { Main } from "@/app/-components";
 import { LockIcon } from "@/components/icons/LockIcon";
 import { Button } from "@/components/ui/button";
 import { UnizikIcon } from "@/components/icons/unizik-logo";
 import { Aeces } from "@/components/icons/aeces";
+import Afam from "@/public/afamHeadshot.jpg";
+import SideBar from "./-components/sideBar";
 
+const cardDetails = [
+	{
+		id: 1,
+		title: "Day 1: Cyber Security Foundation",
+		body: "Estimated time: 10mins",
+		buttonText: "Start Module",
+		isSub: false,
+	},
+	{ id: 4, body: "Day 2", title: <LockIcon />, buttonText: "", isSub: true },
+	{ id: 5, body: "Day 3", title: <LockIcon />, buttonText: "", isSub: true },
+	{ id: 2, title: "Quiz", body: "Complete day 10 to activate", buttonText: "Take Quiz", isSub: false },
+	{
+		id: 3,
+		title: "Certificate",
+		body: "A Certificate of completion would be sent to your E-mail If you pass the minimum mark of 80% in the quiz",
+		buttonText: "Download certificate",
+		isSub: false,
+	},
+];
 const page = () => {
-	const cardDetails = [
-		{
-			id: 1,
-			title: "Day 1: Cyber Security Foundation",
-			body: "Estimated time: 10mins",
-			buttonText: "Start Module",
-			isSub: false,
-		},
-		{ id: 4, body: "Day 2", title: <LockIcon />, buttonText: "", isSub: true },
-		{ id: 5, body: "Day 3", title: <LockIcon />, buttonText: "", isSub: true },
-		{ id: 2, title: "Quiz", body: "Complete day 10 to activate", buttonText: "Take Quiz", isSub: false },
-		{
-			id: 3,
-			title: "Certificate",
-			body: "A Certificate of completion would be sent to your E-mail If you pass the minimum mark of 80% in the quiz",
-			buttonText: "Download certificate",
-			isSub: false,
-		},
-	];
-
 	return (
 		<Main>
 			<div className="flex w-full">
-				{/* SideBar */}
-				<div className="max-h-full w-[50%] bg-cyberaware-aeces-blue" />
+				<SideBar />
 
 				{/* Main Page */}
-				<div className="bg-white px-4 py-4">
+				<div className="flex flex-col gap-3 bg-white px-4 pt-[80px] pb-5">
 					<div className="flex justify-between gap-5">
-						<div>
+						<div className="flex flex-col gap-1">
 							{/* Name and excerpt */}
 							<p className="text-[28px] font-semibold text-[#040524]">Hello, {`Aniekwe`}!</p>
 							<p>You’re on Day {1} of 10</p>
 						</div>
-						<div
+						<Image
+							src={Afam}
+							alt="user"
 							className="h-[50px] w-[50px] rounded-full border-[2px] border-solid
-								border-cyberaware-unizik-orange bg-red-500"
-						>
-							{/* image */}
-						</div>
+								border-cyberaware-unizik-orange"
+						/>
+						{/* image */}
 					</div>
-					<div className="h-2.5 w-full rounded-full bg-cyberaware-unizik-orange" />{" "}
+					<div className="w-full rounded-full bg-[#D9D9D9]">
+						<div className="h-3 w-[10%] rounded-full bg-cyberaware-unizik-orange" />
+					</div>
 					{/* progress bar */}
 					<p>10% complete</p>
 					<div className="flex flex-wrap gap-1">
