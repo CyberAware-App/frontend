c13898f062efbde480236f4a40f82309c559f9bb
Ryan-Zayne <phoenixastro97@gmail.com>
2025-09-03 00:11:45 +0100

    perf(react-query): optimize query client default options
    Disable unnecessary refetching behaviors and increase staleTime to reduce network requests and improve performance

--- lib/react-query/queryClient.ts
+++ lib/react-query/queryClient.ts
@@ -6,9 +6,12 @@ const makeQueryClient = () => {
 		defaultOptions: {
 			queries: {
 				retry: 1,
+				refetchOnWindowFocus: false,
+				refetchOnMount: false,
+				refetchOnReconnect: false,
 				// With SSR, we usually want to set some default staleTime
 				// above 0 to avoid refetching immediately on the client
-				staleTime: 30 * 1000,
+				staleTime: 2 * 60 * 1000,
 			},
 
 			dehydrate: {
